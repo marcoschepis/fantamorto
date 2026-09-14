55767428bb9657e63dc24eedb81158a85555d7a6
Marco <80104162+marcoschepis@users.noreply.github.com>
2026-09-14 16:43:55 +0200

    Implement Google Analytics tracking for page views
    Added Google Analytics event tracking for page views.

--- utils.js
+++ utils.js
@@ -797,6 +797,15 @@ function switchView(name) {
     document.getElementById('btn-' + name).classList.add('active');
 
     currentView = name;
+
+    // Notify google analytics
+    if (typeof gtag === 'function') {
+        gtag('event', 'page_view', {
+            page_title: view,
+            page_path: '/' + view
+        });
+    }
+    
     render();
 }
 
